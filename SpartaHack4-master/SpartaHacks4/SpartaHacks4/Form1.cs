d87f24b1102106dff41405ad2eb96f988afa4597
jaybiillz <58887290+jaybiillz@users.noreply.github.com>
2019-12-14 15:46:55 -0500

    Music

--- SpartaHack4-master/SpartaHacks4/SpartaHacks4/Form1.cs
+++ SpartaHack4-master/SpartaHacks4/SpartaHacks4/Form1.cs
@@ -37,6 +37,10 @@ namespace SpartaHacks4
         Rectangle rMedCircle;
         Rectangle rMedOutline;
         #endregion
+        #region Music
+        bool music;
+        System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+        #endregion
         #region 6ix9ine
 
         Image img = Properties.Resources._69;
@@ -53,6 +57,23 @@ namespace SpartaHacks4
         int totalCols;
 
         Rectangle r69 = new Rectangle();
+        #endregion
+        #region AnimatedBG
+
+        Image img2 = Properties.Resources.BG;
+
+        int count2;
+        int row2;
+        int col2;
+
+        int sheetHeight2;
+        int sheetWidth2;
+        int spriteWidth2;
+        int spriteHeight2;
+        int totalRows2;
+        int totalCols2;
+
+        Rectangle rBG = new Rectangle();
         #endregion
 
 
@@ -85,6 +106,17 @@ namespace SpartaHacks4
             spriteHeight = sheetHeight / totalRows;
 
             r69 = new Rectangle(-50, 375, spriteWidth * 2, spriteHeight * 2);
+            #endregion
+            #region BG
+            img2 = Properties.Resources.BG;
+            sheetWidth2 = img2.Width;
+            sheetHeight2 = img2.Height;
+            totalRows2 = 1;
+            totalCols2 = 32;
+            spriteWidth2 = sheetWidth2 / totalCols2;
+            spriteHeight2 = sheetHeight2 / totalRows2;
+
+            rBG = new Rectangle(0, 0, spriteWidth2 * 2 + 10, spriteHeight2 * 2);
             #endregion
 
         }
@@ -131,6 +163,13 @@ namespace SpartaHacks4
                 }
             }
             #endregion
+            #region music
+            if (music == true)
+            {
+                player = new System.Media.SoundPlayer(Properties.Resources.song);
+
+            }
+            #endregion
 
             this.Refresh();
         }
@@ -145,6 +184,16 @@ namespace SpartaHacks4
 
             // increment the counter
             count += 1;
+
+            if (count2 >= (totalRows2 * totalCols2))
+            {
+                count2 = 0;
+            }
+            row2 = 0;
+            col2 = count2 % totalCols2;  //returns the remainder only (no integer)
+
+            // increment the counter
+            count2 += 1;
 
         }
 
@@ -173,8 +222,17 @@ namespace SpartaHacks4
                 e.Graphics.DrawEllipse(Pens.White, rMedOutline);
             }
             #endregion
+            #region 69
             e.Graphics.DrawImage(img, r69, new RectangleF(col * spriteWidth, row * spriteHeight, spriteWidth, spriteHeight), GraphicsUnit.Pixel);
+            #endregion
+            #region Music
+            if (music == true)
+            {
+                e.Graphics.DrawImage(img2, rBG, new RectangleF(col2 * spriteWidth2, row2 * spriteHeight2, spriteWidth2, spriteHeight2), GraphicsUnit.Pixel);
 
+                e.Graphics.DrawImage(Properties.Resources.MusicText, 0, 450, 1024, 576);
+            }
+            #endregion
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e)
@@ -186,7 +244,8 @@ namespace SpartaHacks4
                 if (rStartButton.Contains(e.Location))
                 {
                     play = true;
-                    meditation = true;
+                    meditation = false;
+                    music = true;
                 }
 
                 if (rChatbot.Contains(e.Location))
@@ -198,7 +257,7 @@ namespace SpartaHacks4
 
         }
 
- 
+
 
 
 
