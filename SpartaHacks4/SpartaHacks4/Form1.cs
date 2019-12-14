2d6e837bed3f39e2bc1104825743faea293a2feb
Brayden01 <36476851+Brayden01@users.noreply.github.com>
2019-12-14 11:46:47 -0500

    Add files via upload

---
+++ SpartaHacks4/SpartaHacks4/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SpartaHacks4
+{
+    public partial class Form1 : Form
+    {
+        #region Start Screen
+
+        //Start Screen Buttons
+        bool play;
+        Rectangle rStartButton;
+        bool chatbot;
+        Rectangle rChatbot;
+        //Start Screen Clouds/Background
+        int cloudVel = 2;
+        Rectangle rCloud1;
+        Rectangle rCloud2;
+        Rectangle rStartBackground;
+
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+             #region Start Screen Initialize
+            //Start Screen Initialization _____________
+            //Buttons
+            rStartButton = new Rectangle(362, 188, 300, 100);
+            rChatbot = new Rectangle(362, 318, 300, 100);
+            //Clouds/Background
+            rCloud1 = new Rectangle(0,0, 1024, 380);
+            rCloud2 = new Rectangle(1024, 0, 1024, 380);
+            rStartBackground = new Rectangle(0, 0, 1024, 576);
+
+            #endregion
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+        #region Start
+            //Cloud Movement
+            rCloud1.X -= cloudVel;
+            rCloud2.X -= cloudVel;
+
+            if (rCloud1.X <= -1024)
+                rCloud1.X = 1024;
+
+            if (rCloud2.X <= -1024)
+                rCloud2.X = 1024;
+            #endregion
+
+            this.Refresh();
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            #region Start Screen Paint
+            //Clouds/Background
+            e.Graphics.DrawImage(Properties.Resources.Background, rStartBackground);
+            e.Graphics.DrawImage(Properties.Resources.Clouds, rCloud1);
+            e.Graphics.DrawImage(Properties.Resources.Clouds, rCloud2);
+            //Draws Start/Chatbot buttons
+            e.Graphics.DrawRectangle(Pens.Black, rStartButton);
+            e.Graphics.DrawRectangle(Pens.Black, rChatbot);
+            #endregion
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            #region Start Screen
+            //Start Screen Button Click
+            if (rStartButton.Contains(e.Location))
+            {
+                play = true;
+            }
+
+            if (rChatbot.Contains(e.Location))
+            {
+                chatbot = true;
+            }
+            #endregion
+
+        }
+
+       
+    }
+}
