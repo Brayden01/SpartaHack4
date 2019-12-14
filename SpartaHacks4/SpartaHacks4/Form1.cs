11064d3dfd95c551630fae570aa6864f3d349639
Brayden01 <36476851+Brayden01@users.noreply.github.com>
2019-12-14 12:19:15 -0500

    Add files via upload

--- SpartaHacks4/SpartaHacks4/Form1.cs
+++ SpartaHacks4/SpartaHacks4/Form1.cs
@@ -19,10 +19,13 @@ namespace SpartaHacks4
         bool chatbot;
         Rectangle rChatbot;
         //Start Screen Clouds/Background
-        int cloudVel = 2;
+        int cloudVel = 4;
         Rectangle rCloud1;
         Rectangle rCloud2;
         Rectangle rStartBackground;
+
+        #endregion
+        #region Happy Scale
 
         #endregion
         public Form1()
@@ -43,16 +46,19 @@ namespace SpartaHacks4
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-        #region Start
-            //Cloud Movement
-            rCloud1.X -= cloudVel;
-            rCloud2.X -= cloudVel;
+        #region Start timer
+            if (play == false)
+            {
+                //Cloud Movement
+                rCloud1.X -= cloudVel;
+                rCloud2.X -= cloudVel;
 
-            if (rCloud1.X <= -1024)
-                rCloud1.X = 1024;
+                if (rCloud1.X <= -1024)
+                    rCloud1.X = 1024;
 
-            if (rCloud2.X <= -1024)
-                rCloud2.X = 1024;
+                if (rCloud2.X <= -1024)
+                    rCloud2.X = 1024;
+            }
             #endregion
 
             this.Refresh();
@@ -62,13 +68,16 @@ namespace SpartaHacks4
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             #region Start Screen Paint
-            //Clouds/Background
-            e.Graphics.DrawImage(Properties.Resources.Background, rStartBackground);
-            e.Graphics.DrawImage(Properties.Resources.Clouds, rCloud1);
-            e.Graphics.DrawImage(Properties.Resources.Clouds, rCloud2);
-            //Draws Start/Chatbot buttons
-            e.Graphics.DrawImage(Properties.Resources.PlayButton, rStartButton);
-            e.Graphics.DrawImage(Properties.Resources.ChatButton, rChatbot);
+            if (play == false)
+            {
+                //Clouds/Background
+                e.Graphics.DrawImage(Properties.Resources.Background, rStartBackground);
+                e.Graphics.DrawImage(Properties.Resources.Clouds, rCloud1);
+                e.Graphics.DrawImage(Properties.Resources.Clouds, rCloud2);
+                //Draws Start/Chatbot buttons
+                e.Graphics.DrawImage(Properties.Resources.PlayButton, rStartButton);
+                e.Graphics.DrawImage(Properties.Resources.ChatButton, rChatbot);
+            }
             #endregion
 
         }
@@ -76,15 +85,18 @@ namespace SpartaHacks4
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
             #region Start Screen
-            //Start Screen Button Click
-            if (rStartButton.Contains(e.Location))
+            if (play == false)
             {
-                play = true;
-            }
+                //Start Screen Button Click
+                if (rStartButton.Contains(e.Location))
+                {
+                    play = true;
+                }
 
-            if (rChatbot.Contains(e.Location))
-            {
-                chatbot = true;
+                if (rChatbot.Contains(e.Location))
+                {
+                    chatbot = true;
+                }
             }
             #endregion
 
